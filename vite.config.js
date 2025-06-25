dc76e20a165388f64e2d93870e40fc57424c61f7
Rocky Ren <rockyrenbread@gmail.com>
2025-06-25 16:16:01 +0800

    patg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 设置相对路径为你的 GitHub 仓库名
+  base: "/INP_DEMO/",
   build: {
     // 指定输出目录为 "docs"
     outDir: "docs",
